38fc5503138b2f7d52c2153cbd8d8696f9a10199
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-07-16 11:16:43 +0200

    init and application.propreties done

---
+++ src/main/java/com/example/checkpoint4/recipeCheckpoint/RecipeCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkpoint4.recipeCheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeCheckpointApplication.class, args);
+	}
+
+}
