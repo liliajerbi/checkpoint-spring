c67b76727d1e8a0d046b2fb3590f50926f1b0245
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-07-17 12:50:14 +0200

    sign up and sign in post done

--- src/main/java/com/example/checkpoint4/recipeCheckpoint/controller/UserController.java
+++ src/main/java/com/example/checkpoint4/recipeCheckpoint/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.example.checkpoint4.recipeCheckpoint.controller;
 
 
+import com.example.checkpoint4.recipeCheckpoint.model.User;
+import com.example.checkpoint4.recipeCheckpoint.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user/signUp")
+    public User createUser (@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/user/signIn")
+    public User signIn (@RequestBody User user) {
+        return userRepository.findUserByEmailIgnoreCaseAndPassword(user.getEmail(), user.getPassword());
+    }
 }
